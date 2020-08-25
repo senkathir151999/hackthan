9c96d7d3116065dfa2e57f95dafdc75355027caa
senkathir15199 <senkathirmathi15@gmail.com>
2020-08-25 09:57:13 +0530

    first commit

---
+++ sass/javascript/pagethre.js
@@ -0,0 +1,14 @@
+
+function btnopen() {
+    document.getElementById("my-modal").style.display = "block";
+}
+function btnclose() {
+    document.getElementById("my-modal").style.display = "none";
+}
+function gridopen() {
+  document.getElementById("second-pop").style.display = "block";
+}
+function goBack() {
+ document.getElementById("my-modal").style.display = "block";
+  document.getElementById("second-pop").style.display = "none";
+}
